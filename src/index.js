6fb1f7943920fd63bb864aa67be9672c9075f14f
Simon Elvery <simon@elvery.net>
2018-12-19 15:16:38 +1000

    Try catch is better

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 module.exports = (tag, noRepeat) => {
   return (err, msg) => {
-    var stderr = (console || {}).error;
-    var logger = (((ABC || {}).News || {}).Logger || {}).log;
+    var stderr;
+    var logger;
+    try {
+      stderr = console.error;
+    } catch (e) {
+      stderr = false;
+    }
+
+    try {
+      logger = ABC.News.Logger.log;
+    } catch (e) {
+      logger = false;
+    }
+
     if (stderr) stderr(err);
-    if (logger) logger(tag, err.name + ': ' + err.message, err, !!noRepeat);
+    if (logger) logger(tag, err.name + ": " + err.message, err, !!noRepeat);
   };
 };
